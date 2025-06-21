4589f6b98faeb41af68fc3a3c19dea598c11e560
Soham170205 <shimpisoham44@gmail.com>
2025-06-21 09:18:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+
+let express = require("express");
+let cors = require("cors");
+let {MongoClient, ObjectId} = require("mongodb");
+let multer = require("multer");
+let path = require("path");
+let fs = require("fs");
+let app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads')); 
+
+const url = "mongodb://0.0.0.0:27017";
+
+
+
+let storage = multer.diskStorage(
+    {
+        destination : (req,file,cb)=>cb(null, "uploads/"),
+        filename : (req,file,cb)=>cb(null, Date.now() + path.extname(file.originalname))
+    }
+);
+
+let recep = multer({storage});
+app.post('/upload', recep.single("file"),(req,res)=>{
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("tinder");
+    let collec = db.collection("photos");
+
+
+let obj = {
+    username : req.body.username,
+    caption : req.body.caption,
+    file_url : `http://localhost:3000/uploads/${req.file.filename}`,
+    file_name : req.file.filename,
+    upload_time : new Date()
+}
+
+collec.insertOne(obj)
+.then((result)=>res.send(result))
+.catch((error)=>res.send(error));
+});
+
+
+
+app.get('/files',(req,res)=>{
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("tinder");
+    let collec = db.collection("photos");
+    let username = req.query.username;
+    let obj = username ? {username} : {}
+
+    collec.find(obj).toArray()
+    .then((result)=>res.send(result))
+    .catch((error)=>res.send(error));
+});
+
+app.delete('/delete/:id',(req,res)=>{
+    let client = new MongoClient(url);
+    client.connect();
+    let db = client.db("tinder");
+    let collec = db.collection("photos");
+    let id = req.params.id;
+    let _id = new ObjectId(id);
+    collec.findOne({_id})
+    .then((obj)=>{
+        fs.promises.unlink(`uploads/${obj.file_name}`)
+        return collec.deleteOne({_id})
+    })
+    .then((result)=>res.send(result))
+    .catch((error)=>res.send(error));
+})
+
+
+app.listen(3000,()=>console.log("Express is Alive"));
+
